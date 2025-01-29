eef55944d55d41b59b5bb31c9ed3b4810ee5cb1c
Devin Cowan <dcowan@cuahsi.org>
2025-01-29 10:00:59 -0500

    Refactor environment variable loading in constants.js and update package name in package-lock.json

--- docs/.vitepress/constants.js
+++ docs/.vitepress/constants.js
@@ -1,7 +1,8 @@
-// import { loadEnv } from 'vitepress'
-// const env = loadEnv('', process.cwd())
+import { loadEnv } from 'vitepress'
+// load env that are exported in the build process
+const env = loadEnv()
 
-export const APP_BASE = import.meta.env.VITE_APP_BASE || '/'
+export const APP_BASE = env.VITE_APP_BASE || '/'
 
-let APP_FULL_URL_IN = import.meta.env.VITE_APP_FULL_URL || 'https://cuahsi-vitepress-example.netlify.app'
+let APP_FULL_URL_IN = env.VITE_APP_FULL_URL || 'https://cuahsi-vitepress-example.netlify.app'
 export const APP_FULL_URL = APP_FULL_URL_IN.endsWith('/') ? APP_FULL_URL_IN : `${APP_FULL_URL_IN}/`
