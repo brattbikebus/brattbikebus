43ac72f823e38494a3871ddfda88e119946a04ef
Devin Cowan <dcowan@cuahsi.org>
2025-09-11 16:27:41 -0400

    add vue-leaflet

--- site/.vitepress/theme/components/Map.vue
+++ site/.vitepress/theme/components/Map.vue
@@ -1,54 +1,29 @@
 <template>
-  <div id="mapContainer"></div>
+  <div style="height:600px; width:800px">
+    <l-map ref="map" v-model:zoom="zoom" :center="[42.8509, -72.5579]">
+      <l-tile-layer
+        url="https://tile.thunderforest.com/cycle/{z}/{x}/{y}.png?apikey=211d97f818704ef0b6a86246e80b4a1f"
+        layer-type="base"
+        name="Thunderforest Cycle"
+        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, &copy; <a href="https://www.thunderforest.com/">Thunderforest</a>'
+      ></l-tile-layer>
+    </l-map>
+  </div>
 </template>
-<script setup>
-import 'leaflet/dist/leaflet.css'
-import L from 'leaflet'
-import { onMounted, ref } from 'vue'
 
-const leaflet = ref(null)
-const control = ref(null)
+<script>
+import "leaflet/dist/leaflet.css";
+import { LMap, LTileLayer } from "@vue-leaflet/vue-leaflet";
 
-onMounted(() => {
-  // https://leafletjs.com/reference.html#map-zoomsnap
-  // https://leafletjs.com/reference.html#map-wheeldebouncetime
-  // https://leafletjs.com/reference.html#map-zoomdelta
-  leaflet.value = L.map('mapContainer', {
-    zoomSnap: 1,
-    wheelDebounceTime: 100,
-    zoomDelta: 1,
-  }).setView([42.8509, -72.5579], 13)
-
-  let CartoDB_PositronNoLabels = L.tileLayer(
-    'https://{s}.basemaps.cartocdn.com/light_nolabels/{z}/{x}/{y}{r}.png',
-    {
-      noWrap: true,
-      attribution:
-        '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>',
-      subdomains: 'abcd'
-    }
-  )
-
-  const baselayers = {
-    'CartoDB Positron No Labels': CartoDB_PositronNoLabels,
-  }
-
-  CartoDB_PositronNoLabels.addTo(leaflet.value)
-
-
-  // /*
-  //  * LEAFLET CONTROLS
-  //  */
-
-
-  // Layer Control
-  control.value = L.control.layers(baselayers).addTo(leaflet.value)
-})
+export default {
+  components: {
+    LMap,
+    LTileLayer,
+  },
+  data() {
+    return {
+      zoom: 13,
+    };
+  },
+};
 </script>
-
-<style scoped>
-#mapContainer {
-  width: 60vw;
-  height: 60vh;
-}
-</style>
