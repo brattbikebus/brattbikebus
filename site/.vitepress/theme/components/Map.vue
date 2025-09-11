a6e47fcb0de18fb111272950e743e496d182f286
Devin Cowan <dcowan@cuahsi.org>
2025-09-11 16:32:03 -0400

    Refactor Map component to use scoped styles and remove unnecessary script options

--- site/.vitepress/theme/components/Map.vue
+++ site/.vitepress/theme/components/Map.vue
@@ -1,6 +1,6 @@
 <template>
-  <div style="height:600px; width:800px">
-    <l-map ref="map" v-model:zoom="zoom" :center="[42.8509, -72.5579]">
+  <div id="mapContainer">
+    <l-map ref="map" zoom="13" :center="[42.8509, -72.5579]">
       <l-tile-layer
         url="https://tile.thunderforest.com/cycle/{z}/{x}/{y}.png?apikey=211d97f818704ef0b6a86246e80b4a1f"
         layer-type="base"
@@ -11,19 +11,14 @@
   </div>
 </template>
 
-<script>
+<script setup>
 import "leaflet/dist/leaflet.css";
 import { LMap, LTileLayer } from "@vue-leaflet/vue-leaflet";
-
-export default {
-  components: {
-    LMap,
-    LTileLayer,
-  },
-  data() {
-    return {
-      zoom: 13,
-    };
-  },
-};
 </script>
+
+<style scoped>
+  #mapContainer {
+    width: 60vw;
+    height: 60vh;
+  }
+</style>
